0a36372049257663e32c44bb57beff22573d4aff
NamKyoungSoo <siz20971@neowiz.com>
2021-03-04 17:18:42 +0900

    hash 값 처리하는 api 추가

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
+import hashlib
 
 app = FastAPI()
 
@@ -10,3 +11,34 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q:Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/hash/{hashType}")
+def getHash(hashType:str, inp:Optional[str]):
+    # TODO : input 값을 parameter로 사용하지 않도록 해야함.
+    hashTypeLower = hashType.lower()
+
+    result = {}
+
+    result['type'] = hashTypeLower
+    result['input'] = inp
+
+    encInp = inp.encode('utf-8')
+    enc = None
+    
+    if hashTypeLower == 'md5':
+        enc = hashlib.md5()
+    elif hashTypeLower == 'sha1':
+        enc = hashlib.sha1()
+    elif hashTypeLower == 'sha256':
+        enc = hashlib.sha256()
+    elif hashTypeLower == 'sha512':
+        enc = hashlib.sha512()
+
+    try:
+        enc.update(encInp)
+        result['output'] = enc.hexdigest()
+        result['hash'] = inp
+    except Exception as ex:
+        result['exception'] = str(ex)
+
+    return result
